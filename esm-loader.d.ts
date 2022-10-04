c6b2fe583c82f4dc3210ae04c5d7542c6401fe4d
jhmaster2000 <32803471+jhmaster2000@users.noreply.github.com>
2022-10-04 07:55:46 -0300

    update to v18.6+ docs

--- esm-loader.d.ts
+++ esm-loader.d.ts
@@ -1,7 +1,20 @@
+/**
+ * Type definitions for custom Node.js ESM loaders
+ * @docs https://nodejs.org/api/esm.html#loaders
+ * @docsversion v18.10.0
+ * @stability 1 - Experimental
+ * @description
+ * To customize the default module resolution, loader hooks can optionally be
+ * provided via a `--experimental-loader ./loader-name.mjs` argument to Node.js.
+ * 
+ * When hooks are used they apply to the entry point and all `import` calls.
+ * They won't apply to `require` calls; those still follow _CommonJS_ rules.
+ */
 declare module 'typings-esm-loader' {
 
 // @ts-expect-error tsserver sometimes glitches out and complains about the 'declare global', but still works fine
 declare global {
+    /** An ESM loader when imported as a module. */
     interface Loader {
         resolve?: resolve;
         load?: load;
@@ -9,78 +22,122 @@ declare global {
         [key: string]: unknown;
     }
 
-    type TypedArray = Int8Array | Uint8Array | Uint8ClampedArray | Int16Array | Uint16Array | Int32Array | Uint32Array | Float32Array | Float64Array | BigInt64Array | BigUint64Array;
-
     /**
      * `format` | Description | Acceptable types for `source` returned by `load`
      * -|-|-
      * `'builtin'` | Load a Node.js builtin module | Not applicable
      * `'commonjs'` | Load a Node.js CommonJS module | Not applicable
-     * `'json'` | Load a JSON file | { {@linkcode String string}, {@linkcode ArrayBuffer}, {@linkcode TypedArray} }
-     * `'module'` | Load an ES module | { {@linkcode String string}, {@linkcode ArrayBuffer}, {@linkcode TypedArray} }
-     * `'wasm'` | Load a WebAssembly module | { {@linkcode ArrayBuffer}, {@linkcode TypedArray} }
+     * `'json'` | Load a JSON file | { {@linkcode String string}, {@linkcode ArrayBuffer}, {@linkcode Uint8Array TypedArray} }
+     * `'module'` | Load an ES module | { {@linkcode String string}, {@linkcode ArrayBuffer}, {@linkcode Uint8Array TypedArray} }
+     * `'wasm'` | Load a WebAssembly module | { {@linkcode ArrayBuffer}, {@linkcode Uint8Array TypedArray} }
      */
     type ModuleFormat = 'builtin' | 'commonjs' | 'json' | 'module' | 'wasm';
 
     /**
-     * The `resolve` hook returns the resolved file URL for a given module specifier and parent URL, and optionally its format (such as `'module'`) as a hint to the `load` hook.
+     * The `resolve` hook chain is responsible for resolving file URL for a given module specifier and parent URL,
+     * and optionally its format (such as `'module'`) as a hint to the `load` hook.
      * 
-     * If `resolve` provides a `format`, a custom `load` hook is required even if only to pass the value to the Node.js default `load` hook.
+     * If a format is specified, the `load` hook is ultimately responsible for providing the final `format` value
+     * (and it is free to ignore the hint provided by `resolve`); if `resolve` provides a `format`,
+     * a custom `load` hook is required even if only to pass the value to the Node.js default `load` hook.
+     * 
      * @param specifier The string in an `import` statement or `import()` expression.
+     * @param nextResolve The subsequent `resolve` hook in the chain, or the Node.js default `resolve` hook after the last user-supplied `resolve` hook
      */
-    type resolve = (specifier: string, context: Resolve.Context, defaultResolve: Resolve.Function) => Promise<Resolve.Return>;
+    type resolve = (specifier: string, context: Resolve.Context, nextResolve: Resolve.Function) => Promise<Resolve.Return>;
 
     namespace Resolve {
         interface Context {
-            /** An array of conditions for {@link https://nodejs.org/api/packages.html#conditional-exports package exports conditions} that apply to this resolution request.
+            /** Export conditions of the relevant `package.json`
              * 
-             *  They can be used for looking up conditional mappings elsewhere or to modify the list when calling the default resolution logic. */
+             * An array of conditions for {@link https://nodejs.org/api/packages.html#conditional-exports package exports conditions} that apply to this resolution request.
+             * 
+             * They can be used for looking up conditional mappings elsewhere or to modify the list when calling the default resolution logic. */
             conditions: string[];
+            /** @undocumented */
+            importAssertions: Record<string, unknown>;
             /** The URL of the module that imported this one, or `undefined` if this is the main entry point for the application. */
-            parentURL: string | undefined;
-            //!@upcoming importAssertions: object;
+            parentURL?: string;
         }
 
         interface Return {
-            /** The absolute url to the import target (such as `file://…`) */
-            url: string;
-            /** - See {@link ModuleFormat} */
+            /** A hint to the load hook (it might be ignored) `'builtin' | 'commonjs' | 'json' | 'module' | 'wasm'`
+             * @see {@link ModuleFormat} */
             format?: ModuleFormat | string | null;
+            /** A signal that this hook intends to terminate the chain of `resolve` hooks.
+             * @default false */
+            shortCircuit?: boolean;
+            /** The absolute URL to which this input resolves */
+            url: string;
         }
 
-        /**
-         * @param specifier The string in an `import` statement or `import()` expression.
-         */
-        type Function = (specifier: string, context: Resolve.Context, resolver: Resolve.Function) => Promise<Resolve.Return>;
+        /** Same as {@linkcode resolve} hook but without the `nextResolve` parameter. */
+        type Function = (specifier: string, context?: Partial<Resolve.Context>) => Promise<Resolve.Return>;
     }
 
     /**
-     * The `load` hook provides a way to define a custom method of determining how a URL should be interpreted, retrieved, and parsed. It is also in charge of validating the import assertion.
-     * @param context If `resolve` settled with a `format`, that value is included here.
+     * The `load` hook provides a way to define a custom method of determining how a URL should be interpreted, retrieved, and parsed.
+     * It is also in charge of validating the import assertion.
+     * @param url The URL returned by the `resolve` chain
+     * @param nextLoad The subsequent `load` hook in the chain, or the Node.js default `load` hook after the last user-supplied `load` hook
      */
-    type load = (url: string, context: Load.Context, defaultLoad: Load.Function) => Promise<Load.Return>;
+    type load = (url: string, context: Load.Context, nextLoad: Load.Function) => Promise<Load.Return>;
 
     namespace Load {
         interface Context {
-            /** The format optionally supplied by the `resolve` hook. See {@link ModuleFormat} */
-            format: ModuleFormat | string | null | undefined; 
-            //!@upcoming importAssertions: object; 
+            /** Export conditions of the relevant `package.json` */
+            conditions: string[];
+            /** The format optionally supplied by the `resolve` hook chain
+             * @see {@link ModuleFormat} */
+            format?: ModuleFormat | string | null;
+            /** @undocumented */
+            importAssertions: Record<string, unknown>; 
         }
 
         interface Return {
-            /** - See {@link ModuleFormat} */
+            /** The final value of `format` must be one of the following:
+             * @see {@link ModuleFormat} */
             format: ModuleFormat;
-            source: string | ArrayBuffer | SharedArrayBuffer | TypedArray | Buffer;
+            /** A signal that this hook intends to terminate the chain of `load` hooks.
+             * @default false */
+            shortCircuit?: boolean;
+            /** The source for Node.js to evaluate
+             * 
+             * If the source value of a text-based format (i.e., `'json'`, `'module'`) is not a string,
+             * it is converted to a string using {@linkcode https://nodejs.org/api/util.html#class-utiltextdecoder util.TextDecoder}.
+             */
+            source: string | ArrayBuffer | SharedArrayBuffer | Uint8Array;
         }
 
-        type Function = (url: string, context: Load.Context, loader: Load.Function) => Promise<Load.Return>;
+        /** Same as {@linkcode load} hook but without the `nextLoad` parameter. */
+        type Function = (url: string, context?: Partial<Load.Context>) => Promise<Load.Return>;
     }
 
     /**
-     * Sometimes it might be necessary to run some code inside of the same global scope that the application runs in. This hook allows the return of a string that is run as a sloppy-mode script on startup.
+     * Sometimes it might be necessary to run some code inside of the same global scope that the application runs in.
+     * This hook allows the return of a string that is run as a sloppy-mode script on startup.
+     * 
+     * Similar to how CommonJS wrappers work, the code runs in an implicit function scope.
+     * The only argument is a `require`-like function that can be used to load builtins like "fs": {@linkcode getBuiltin getBuiltin(request: string)}.
+     * 
+     * If the code needs more advanced `require` features, it has to construct its own `require` using `module.createRequire()`.
+     * @param context Information to assist the preload code
      * @returns Code to run before application startup
      */
-    type globalPreload = (/** @upcoming utilities: { port: MessagePort } - Things that preload code might find useful */) => string;
+    type globalPreload = (context: {
+        /** 
+         * In order to allow communication between the application and the loader,
+         * another argument is provided to the preload code: `port`.
+         * 
+         * This is available as a parameter to the loader hook and inside of the source text returned by the hook.
+         * 
+         * Some care must be taken in order to properly call
+         * {@linkcode https://nodejs.org/dist/latest-v17.x/docs/api/worker_threads.html#portref port.ref()} and
+         * {@linkcode https://nodejs.org/dist/latest-v17.x/docs/api/worker_threads.html#portunref port.unref()}
+         * to prevent a process from being in a state where it won't close normally.
+         */
+        port: MessagePort
+    }) => string;
 
     /**
      * A `require`-like function that can be used to load builtins like "fs"
